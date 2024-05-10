a9fef2967cf260d807e9a4cefb89f0af415d03a0
D4wZ7z <166541999+D4wZ7z@users.noreply.github.com>
2024-05-10 16:18:04 +0200

    Update Searcher By D4wZ7z.py

--- Searcher By D4wZ7z.py
+++ Searcher By D4wZ7z.py
@@ -33,80 +33,77 @@ def print_logo():
     print(Colorate.Horizontal(Colors.rainbow,"+---------------Searcher---------------+"))
 
 
-def search_files(keyword, directory="."):
+def search_files(keyword, directory="db"):
     found_entries = []
     start_time = time.time()
+    valid_extensions = (".txt", ".sql")  # possible d'ajouter d'autre extension de fichier si vous voulais.
     
     for root, dirs, files in os.walk(directory):
         for file in files:
-            if file.endswith(".txt", ".sql"):
+            if file.endswith(valid_extensions):  
                 file_path = os.path.join(root, file)
                 matching_lines = []  
-                with open(file_path, 'r', encoding='latin1') as f:
-                    for line_number, line in enumerate(f, start=1):
-                        if keyword in line:
-                            entry = {
-                                "file_path": os.path.relpath(file_path, directory),
-                                "line_number": line_number,
-                                "line_content": line.strip()
-                            }
-                            found_entries.append(entry)
-                            matching_lines.append(line.strip())  
-                if matching_lines:
-                    print(Colorate.Horizontal(Colors.green_to_red,(f"Word found in file : {file_path}")))
-                    for line in matching_lines:
-                        print(Colorate.Horizontal(Colors.green_to_yellow,(f"Ligne {line_number}: {line}")))
-                    print()  
+                try:
+                    with open(file_path, 'r', encoding='latin1') as f:
+                        for line_number, line in enumerate(f, start=1):
+                            if keyword in line:
+                                entry = {
+                                    "file_path": os.path.relpath(file_path, directory),
+                                    "line_number": line_number,
+                                    "line_content": line.strip()
+                                }
+                                found_entries.append(entry)
+                                matching_lines.append(line.strip())  
+                    if matching_lines:
+                        print(Colorate.Horizontal(Colors.green_to_red, (f"Word found in file : {file_path}")))
+                        for line in matching_lines:
+                            print(Colorate.Horizontal(Colors.green_to_yellow, (f"Ligne {line_number}: {line}")))
+                        print()  
+                except FileNotFoundError:
+                    print(Colorate.Horizontal(Colors.red_to_blue, (f"File not found: {file_path}. Skipping...")))
 
     end_time = time.time()
     search_duration = end_time - start_time
     return found_entries, search_duration
-
-
 
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def main():
     clear_screen()
-    root_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), "DB")
-    
-    if not os.path.exists(root_directory) or not os.path.isdir(root_directory):
-        print(Colorate.Horizontal(Colors.purple_to_blue,("The DB directory does not exist.")))
-        return
-    
-    menu_text = ""
-    print_logo()           
-    menu = (menu_text)
-    print("\n".join(menu.split("\n")[:-1]).center(os.get_terminal_size().columns))
-    print(menu.split("\n")[-1])
-
+    print_logo()
+    print()
+    print()
+    print(Colorate.Horizontal(Colors.rainbow,"Type the word you wish to search for and press Enter ('Q' to exit and 'cls' to clear cmd) : "))
+    print()
     while True:
-        print()
-        print()
-        keyword = input(Colorate.Horizontal(Colors.red_to_blue,("\nType the word you wish to search for and press Enter ('Q' to exit and 'cls' to clear cmd ) : ")))
-                    
-        
-        if keyword.lower() == "q":
+        keyword = input()
+        if keyword.lower() == 'q':
             break
-        else:
-            if keyword.lower() == "cls":
-                clear_cmd()
-
         
+        root_directory = "db"
         entries, search_duration = search_files(keyword, root_directory)
-
         if entries:
-            print("\nRésultats :")
+            print()
+            print()
+            print("\nResults:")
+            print()
+            print()
             for entry in entries:
-                print(Colorate.Horizontal(Colors.red_to_yellow,(f"Word found in file : {entry['file_path']}")))
-                print()
-                print(Colorate.Horizontal(Colors.green_to_cyan,(f"Line {entry['line_number']}: {entry['line_content']}\n")))
                 print()
                 print()
-            print(Colorate.Horizontal(Colors.rainbow,(f"Search time : {search_duration:.2f} secondes")))
+                print(f"Word found in file: {entry['file_path']}")
+                print()
+                print()
+                print(f"Line {entry['line_number']}: {entry['line_content']}\n")
+                print()
+                print()
+            print(f"Search time: {search_duration:.2f} seconds")
+            print()
+            print()
         else:
-            print(Colorate.Horizontal(Colors.red_to_blue,("\nNo results found.")))
+            print()
+            print("\nNo results found.")
 
 if __name__ == "__main__":
     main()
